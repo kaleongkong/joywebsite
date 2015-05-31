af93761e24dc20ae994224e2d9b6b573ac3052ea
kaleong kong <kaleongkong@yahoo.com>
2015-05-31 16:50:12 -0700

    update form

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,14 @@
 class UserMailer < ActionMailer::Base
   default from: "joyrealestate.sup@gmail.com"
-  def send_email
+  def send_email(args={})
+  	@desired_st_addr = args[:desired_st_addr]
+  	@desired_city = args[:desired_city]
+  	@desired_state = args[:desired_state]
+  	@desired_zip = args[:desired_zip]
+  	@price_range = args[:price_range]
+  	@size = args[:size]
+  	@desired_property_type = {single_family: args[:single_family], condo: args[:condo], multiunits: args[:multiunits]}
+  	@desired_property_type.each{|k,v| @desired_property_type if v}.compact!
   	mail(to: "joyrealestate.ad@gmail.com", subject: 'Sample Email')
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
   # config.action_mailer.raise_delivery_errors = false
@@ -44,8 +44,8 @@ Rails.application.configure do
    :address              => "smtp.gmail.com",
    :port                 => 587,
    :domain               => 'localhost:3000',
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
+   :user_name            => 'joyrealestate.sup@gmail.com',
+   :password             => 'joyrealestate',
    :authentication       => :plain,
   }
 end
